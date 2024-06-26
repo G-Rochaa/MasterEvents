e8005820dd392d30b0fbcda97917266082e995d7
Gustavo R. <agect.gustavo@gmail.com>
2024-06-26 16:43:20 -0300

    Aplicando modificações na parte Web (filtros e design).

--- Mjölnir.Web/MasterEvents-Web/src/app/app.module.ts
+++ Mjölnir.Web/MasterEvents-Web/src/app/app.module.ts
@@ -2,23 +2,30 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http'
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EventosComponent } from './eventos/eventos.component';
 import { PalestrantesComponent } from './Palestrantes/Palestrantes.component';
+import { NavComponent } from './nav/nav.component';
+
+import { CollapseModule } from 'ngx-bootstrap/collapse';
 
 @NgModule({
-  declarations: [	
+  declarations: [		
     AppComponent,
     EventosComponent,
-    PalestrantesComponent
+    PalestrantesComponent,
+    NavComponent
    ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    BrowserAnimationsModule 
+    BrowserAnimationsModule,
+    CollapseModule.forRoot(),
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- Mjölnir.Web/MasterEvents-Web/src/app/eventos/eventos.component.ts
+++ Mjölnir.Web/MasterEvents-Web/src/app/eventos/eventos.component.ts
@@ -8,7 +8,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EventosComponent implements OnInit {
 
-  public eventos: any;
+  public eventos: any = [];
+  public eventosFiltrados: any = [];
+  widthImg: number = 180;
+  marginImg: number = 10;
+  showImage: boolean = true;
+  private _filtroLista: string = "";
+
+  public get filtroLista() : string{
+    return this._filtroLista;
+  }
+
+  public set filtroLista(value: string){
+    this._filtroLista = value;
+    this.eventosFiltrados = this.filtroLista ? this.filtrarEventos(this.filtroLista) : this.eventos; 
+  }
+
+
+  filtrarEventos(filtrarPor: string): any{
+    filtrarPor =  filtrarPor.toLocaleLowerCase();
+    return this.eventos.filter(
+      (evento : any) => evento.tema.toLocaleLowerCase().indexOf(filtrarPor) !==-1 ||
+      evento.local.toLocaleLowerCase().indexOf(filtrarPor) !== -1
+    )
+  }
 
   constructor(private http: HttpClient) { }
 
@@ -16,13 +39,20 @@ export class EventosComponent implements OnInit {
     this.getEventos();
   }
 
+  alterarEstadoImagem(){
+    this.showImage = !this.showImage;
+  }
+
   public getEventos(): void{
-    this.http.get('https://localhost:44350/api/eventos').subscribe(
-      response => this.eventos = response,
-      error => console.log(error)
-    );
+     this.http.get('https://localhost:44350/api/eventos').subscribe(
+       response => {
+        this.eventos = response
+        this.eventosFiltrados = this.eventos;
+       },
+       error => console.log(error)
+     );
 
 
   }
 
-}
+} 
